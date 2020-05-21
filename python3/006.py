2b4138a96f82233f1c8bacf07a45535a1e980599
Scott R.S <36688292+arandomsapien@users.noreply.github.com>
2020-05-20 18:11:23 -0700

    Solution to #6

---
+++ python3/006.py
@@ -0,0 +1,30 @@
+# Solution to problem 6
+# https://projecteuler.net/problem=5
+
+def sum_of_squares(n):
+    """ returns the sum of squares of first n numbers """
+
+    iter = 1
+    
+    sum = 0
+
+    while iter <= n:
+        sum += iter**2
+        iter += 1
+    
+    return sum
+
+def square_of_sums(n):
+    """ returns the square of sums of first n numbers """
+
+    iter = 1
+    
+    sum = 0
+
+    while iter <= n:
+        sum += iter
+        iter += 1
+    
+    return sum**2
+
+print(square_of_sums(100) - sum_of_squares(100))
